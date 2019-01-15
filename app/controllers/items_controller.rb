b483c51b722789a6851e8f9c62af745dc0d6ba95
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-01-15 13:32:25 +0900

    add_image_function1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,18 @@
 class ItemsController < ApplicationController
+  def index
+    @items
+  end
+
+  def new
+  end
+
+  def create
+    Item.create(image: item_params[:image])
+  end
+
+  private
+  def items_params
+    params.permit(:image)
+  end
+
 end
