eded0bfbf9091f79a6bfedac1a39dc8ccabc6495
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-01-15 16:20:30 +0900

    add_basic2
    ローカルでエラーが出たため修正した。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   private
   def production?
     Rails.env.production?
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,6 @@
 class ItemsController < ApplicationController
+
   def index
-    @items
-  end
-
-  def new
-  end
-
-  def create
-    Item.create(image: item_params[:image])
-  end
-
-  private
-  def items_params
-    params.permit(:image)
   end
 
 end
