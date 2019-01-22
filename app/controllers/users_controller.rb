163325fe684ca5ceace4eba00364122e3c3556a4
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-01-22 21:15:34 +0900

    Edit routes and users controller
    Users controller にeditアクションを追加し、routeにもeditを追加した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
   def show
   end
+  def edit
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   root  'items#index'
   resources :items, only: [:index,:new,:create]
-  resources :users, only: [:show]
+  resources :users, only: [:show,:edit]
 end
