fbe1378644d4349124350af2ffd6709eee74317c
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-01-24 11:51:15 +0900

    deproy11
    Basic認証の解除

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 server '13.112.3.10', user: 'ec2-user', roles: %w{app db web}
 
-set :rails_env, "production"
-set :unicorn_rack_env, "production"
+# set :rails_env, "production"
+# set :unicorn_rack_env, "production"
 
 # server-based syntax
 # ======================
