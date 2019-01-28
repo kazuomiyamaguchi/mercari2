e7a04845ac1625a3b500cca2ae68557939a95264
NasuYayoi <ynynyn1014@gmail.com>
2019-01-28 17:56:30 +0900

    add column users and setting validation
    ユーザモデルにカラムを追加。バリデーションとアソシエーションを定義。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true, uniqueness: true
+  validates :phone_number, presence: true, uniqueness: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+
+  has_one :address, dependent: :destroy
+  accepts_nested_attributes_for :address
+  has_one :creditcard, dependent: :destroy
+  accepts_nested_attributes_for :creditcard
 end
