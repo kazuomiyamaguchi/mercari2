4759e3359c50b63897e678e0dbb8fc178c6ff2c9
kazuomiyamaguchi <kazuomiyamaguchi1110@gmail.com>
2019-01-28 16:45:39 +0900

    edit routes.rb
    buyアクションのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-
   root  'items#index'
-  resources :items, only: [:index, :show, :new, :create, :buy]
+  get 'items/buy' => "items#buy"
+  resources :items, only: [:index, :show, :new, :create]
   resources :users, only: [:show, :edit]
   get 'user/logout' => "users#logout"
 end
