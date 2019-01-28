429aef42bce83ff6ccb08d2484018f52642dbc6a
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-01-28 13:53:54 +0900

    Create routes and controller
    Items コントローラにsellアクションを作成し、ルートで繋いだ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+  def create
+  end
+  def sell
+    @item = Prefecture.new
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
 
   root  'items#index'
   resources :items, only: [:index, :new, :create]
+  get 'items/sell' => 'items#sell'
 
   resources :users,only: [:show, :edit]
   get 'user/prof' => 'users#prof'
   get 'user/mypage' => 'users#mypage'
 
   resources :users, only: [:show, :edit]
-  get 'user/logout' => "users#logout"
+  get 'user/logout' => 'users#logout'
 
 end
