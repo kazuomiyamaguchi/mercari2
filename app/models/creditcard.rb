08b0cd45c2e4864c027f52d34580777a1be8b41d
NasuYayoi <ynynyn1014@gmail.com>
2019-01-28 19:15:15 +0900

    modify validation creditcard.rb
    Creditcardモデルのバリデーションの記述を一行にまとめた。
    （presence:trueだけのカラムに対して）

--- app/models/creditcard.rb
+++ app/models/creditcard.rb
@@ -1,9 +1,7 @@
 class Creditcard < ApplicationRecord
   belongs_to :user, optional: true
 
-  validates :number, presence: true
-  validates :expiration_month, presence: true
-  validates :expiration_year, presence: true
+  validates :number, :expiration_month, :expiration_year, presence: true
   validates :security_code, presence: true, format: { with: /\A\d{3,4}\z/ }
 
 end
