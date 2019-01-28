bdfa46d602bc8ae4e893cd27521794ea1f87e55f
NasuYayoi <ynynyn1014@gmail.com>
2019-01-29 00:39:55 +0900

    Modify routes.rb
    タイプミスを修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit]
   get 'user/prof' => 'users#prof'
   get 'user/mypage' => 'users#mypage'
-  get 'user/credit' => 'user#credit'
+  get 'user/credit' => 'users#credit'
   get 'user/logout' => "users#logout"
 
 end
