3d36768ec3878012cc982bc6520b74000b94ab1a
kazuomiyamaguchi <kazuomiyamaguchi1110@gmail.com>
2019-01-28 16:14:57 +0900

    create buy.html.haml&buy.scss
    Viewファイルの作成
    Scssファイルの作成
    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root  'items#index'
-  resources :items, only: [:index, :show, :new, :create]
+  resources :items, only: [:index, :show, :new, :create, :buy]
   resources :users, only: [:show, :edit]
   get 'user/logout' => "users#logout"
 end
