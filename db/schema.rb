bda79ca2873ad746aa870cabb230f2bc3d235fb4
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-01-28 13:56:21 +0900

    Create prefecture table
    都道府県セレクタを作成するためにprefecute tableを作成し、enumの設定を行った。

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190118104938) do
+ActiveRecord::Schema.define(version: 20190127031534) do
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "image"
+  end
+
+  create_table "prefectures", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
