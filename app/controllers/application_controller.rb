e10332b35a4fbaf82f82968e8d9623c788c03dce
NasuYayoi <ynynyn1014@gmail.com>
2019-01-28 18:21:26 +0900

    Setting configure_permitted_parameters
    Userモデルに追加したカラムをconfigure_permitted_parametersに追加。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :phone_number, address_attributes: [:postcode, :prefecture_id, :city, :address, :building_name], creditcard_attributes: [:number, :expiration_month, :expiration_year, :security_code]])
   end
 
   private
