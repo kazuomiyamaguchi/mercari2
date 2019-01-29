2c58916404dae9e041ea0e4cd9524fe1c7edbc4d
kazuomiyamaguchi <kazuomiyamaguchi1110@gmail.com>
2019-01-29 19:18:48 +0900

    Change className
    可読性を上げるために、jsで利用するクラス名の変更

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -5,16 +5,16 @@ $(function() {
       items: 1, // 1画面に表示するアイテム数
     });
 
-    $(".dot1").on('click', function () {
+    $(".js-dot1").on('click', function () {
         owl.trigger('to.owl.carousel', 0);
     })
-    $(".dot2").on('click', function () {
+    $(".js-dot2").on('click', function () {
         owl.trigger('to.owl.carousel', 1);
     })
-    $(".dot3").on('click', function () {
+    $(".js-dot3").on('click', function () {
         owl.trigger('to.owl.carousel', 2);
     })
-    $(".dot4").on('click', function () {
+    $(".js-dot4").on('click', function () {
         owl.trigger('to.owl.carousel', 3);
     });
   });
