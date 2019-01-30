d4c16b185294682d54ff7e2108f2d2c4ea548885
NasuYayoi <ynynyn1014@gmail.com>
2019-01-30 20:47:31 +0900

    fix: デプロイ時にマイグレーションファイルが実行できないエラーを修正
    デプロイ時、マイグレーションファイルを実行した際、生じたエラー対応のため、マイグレーションファイルを修正。
    参照先のテーブルがないのに外部キーを参照するカラムを作成しようとしていたために参照整合性が取れずに生じたエラー。

--- db/migrate/20190125082237_add_column_to_users.rb
+++ db/migrate/20190125082237_add_column_to_users.rb
@@ -5,8 +5,5 @@ class AddColumnToUsers < ActiveRecord::Migration[5.0]
     add_column    :users, :last_name_kana,  :string,     null: false
     add_column    :users, :first_name_kana, :string,     null: false
     add_column    :users, :phone_number,    :string,     null: false, unique: true
-    add_reference :users, :address, foreign_key: true
-    add_reference :users, :payment, foreign_key: true
-    add_reference :users, :creditcard, foreign_key: true
   end
 end
--- db/migrate/20190128040004_remove_payments.rb
+++
@@ -1,5 +0,0 @@
-class RemovePayments < ActiveRecord::Migration[5.0]
-  def change
-    drop_table:payments
-  end
-end
