d29c871291f93e67f9f6da75ae2d30e7403fa9b0
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-02-02 14:26:21 +0900

    Deproy25

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190129080532) do
+ActiveRecord::Schema.define(version: 20190125082133) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "postcode",      null: false
@@ -20,17 +20,7 @@ ActiveRecord::Schema.define(version: 20190129080532) do
     t.string   "building_name"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.integer  "user_id"
     t.index ["prefecture_id"], name: "index_addresses_on_prefecture_id", using: :btree
-    t.index ["user_id"], name: "index_addresses_on_user_id", using: :btree
-  end
-
-  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "ancestry"
-    t.string   "name"
-    t.index ["ancestry"], name: "index_categories_on_ancestry", using: :btree
   end
 
   create_table "creditcards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -45,14 +35,6 @@ ActiveRecord::Schema.define(version: 20190129080532) do
   end
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "category"
-    t.integer  "category_id"
-  end
-
-  create_table "prefectures", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -66,16 +48,10 @@ ActiveRecord::Schema.define(version: 20190129080532) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "nickname"
-    t.string   "last_name",                           null: false
-    t.string   "first_name",                          null: false
-    t.string   "last_name_kana",                      null: false
-    t.string   "first_name_kana",                     null: false
-    t.string   "phone_number",                        null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["nickname"], name: "index_users_on_nickname", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "addresses", "users"
   add_foreign_key "creditcards", "users"
 end
