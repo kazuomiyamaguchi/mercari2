837f962c8795a2a3ee340a0bbc0248a61c687111
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-02-02 16:19:55 +0900

    Deproy32

--- db/migrate/20190125082237_add_column_to_users.rb
+++ db/migrate/20190125082237_add_column_to_users.rb
@@ -4,6 +4,6 @@ class AddColumnToUsers < ActiveRecord::Migration[5.0]
     add_column    :users, :first_name ,     :string,     null: false
     add_column    :users, :last_name_kana,  :string,     null: false
     add_column    :users, :first_name_kana, :string,     null: false
-    # add_column    :users, :phone_number,    :string,     null: false
+    add_column    :users, :phone_number,    :string,     null: false
   end
 end
