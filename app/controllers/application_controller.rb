1279c736222d530087b67a61d2c02f72d1deae0c
kazuomiyamaguchi <kazuomiyamaguchi1110@gmail.com>
2019-02-05 00:15:28 +0900

    Add Ransack To SearchForm
    検索枠にRansackの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,14 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_search
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :phone_number, address_attributes: [:postcode, :prefecture_id, :city, :address, :building_name], creditcard_attributes: [:number, :expiration_month, :expiration_year, :security_code]])
+  end
+
+  def set_search
+    @q = Item.search(params[:q])
   end
 
   private
