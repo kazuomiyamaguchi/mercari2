0e4e0e61f3b65b23c4c8f87c3f79c6f1d67d5319
kazuomiyamaguchi <kazuomiyamaguchi1110@gmail.com>
2019-02-04 22:34:18 +0900

    Add faker
    ダミーデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+# faker
+20.times do
+     Item.create(
+        name: Faker::Commerce.product_name,
+        price: Faker::Commerce.price,
+        category_id: Faker::Number.between(1, 10)
+      )
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
