11a479c5c55bfc7097972d6114bee8961abd9e4e
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-02-04 18:37:21 +0900

    Deproy37

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20190204051647) do
-
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "postcode",      null: false
