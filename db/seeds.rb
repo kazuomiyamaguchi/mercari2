1926cd91643e7b269b49b6e42e86327a0b543fe8
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-02-05 18:51:30 +0900

    Deproy38

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 # faker
-20.times do
-     Item.create(
-        name: Faker::Commerce.product_name,
-        price: Faker::Commerce.price,
-        category_id: Faker::Number.between(1, 10)
-      )
-end
+# 20.times do
+#      Item.create(
+#         name: Faker::Commerce.product_name,
+#         price: Faker::Commerce.price,
+#         category_id: Faker::Number.between(1, 10)
+#       )
+# end
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
